6f10eeb9dc9872f563b00cf584349c84b98c701f
Artur Semenas <uncelfuka@gmail.com>
2023-12-22 12:08:52 +0100

    Stage 1 Lambda Expressions

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,19 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        Operation<Integer> op = x -> {
+            List<Integer> list = new ArrayList<>();
+            for(int i : x){
+            list.add(i/divider);
+            }
+            return list;
+        };
+        return op;
     }
 }
